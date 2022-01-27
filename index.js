f0f9267b13cdf51af0d55ff7573fb6850973bfbd
Denis Golubev <96134655+GuiltyEND@users.noreply.github.com>
2022-01-27 16:21:38 +0200

    Add files via upload
    second try

--- index.js
+++ index.js
@@ -1,110 +1,194 @@
-// 'use strict'
+
 // 1
-// export function basicOp(operation, a, b,) 
-// {
-//     if (operation == '+')
-//         {
-//         return a + b; 
-//         }
-//     else if (operation == '-')
-//         {
-//         return a - b;
-//         }
+export function basicOp(operation, a, b,) 
+{
+    if (operation == '+')
+        {
+        return a + b; 
+        }
+    else if (operation == '-')
+        {
+        return a - b;
+        }
 
-//     else if (operation == '*')
-//         {
-//         return a * b; 
-//         }
+    else if (operation == '*')
+        {
+        return a * b; 
+        }
 
-//     else if (operation == '/') 
-//         {
-//         return a / b; 
-//         }
+    else if (operation == '/') 
+        {
+        return a / b; 
+        }
 
-//     else 
-//         { 
-//         return 0 ;
-//         }
+    else 
+        { 
+        return 0 ;
+        }
 
-//     let result;
+    let result;
   
-//     return result;
-// }
+    return result;
+}
 
-// console.log(basicOp('+', 4, 7));
-// console.log(basicOp('-', 15, 18));
-// console.log(basicOp('*', 5, 5)) ;
-// console.log(basicOp('/', 49, 7));
-// console.log(basicOp('something else', 45, 45)) ;
+console.log(basicOp('+', 4, 7));
+console.log(basicOp('-', 15, 18));
+console.log(basicOp('*', 5, 5)) ;
+console.log(basicOp('/', 49, 7));
+console.log(basicOp('something else', 45, 45)) ;
 
 // 2
 
-// export function stringMerge(first, second, letter) {
-//     let num = first.indexOf(letter)
-//     first = first.slice(0, num)
-//     num = second.indexOf(letter)
-//     second = second.slice(num + 1, second.length)
+export function stringMerge(first, second, letter) {
+    const num = first.indexOf(letter)
+    first = first.slice(0, num)
+    num = second.indexOf(letter)
+    second = second.slice(num + 1)
 
-//     return first + letter + second  
-// }
-
-
+    return first + letter + second  
+}
 
 
-// function stringMerge(first,second,letter){
-//     first=first.split('');
-//     second=second.split('');
-//     for(let i = 0; i<= first.length; i++){
-//         if (first[i]==letter){
-//           first=first.slice(0,i+1);
-//           break;  
-//         }
-//     }
-//     for(let i = 0; i<= second.length; i++){
-//         if (second[i]==letter){
-//             first=first+second.slice(i+1,second.length)
-//             first=first.split(',');
-//           break;  
-//         }
-//     }
-//     first=first.join('');
-//     return first;
-// }
-// console.log( stringMerge("hello", "world", "l"));
-// console.log( stringMerge("coding", "anywhere", "n") )
-// console.log( stringMerge("jason", "samson", "s"))
+
+
+function stringMerge(first,second,letter){
+    first=first.split('');
+    second=second.split('');
+    for(let i = 0; i<= first.length; i++){
+        if (first[i]==letter){
+          first=first.slice(0,i+1);
+          break;  
+        }
+    }
+    for(let i = 0; i<= second.length; i++){
+        if (second[i]==letter){
+            first=first+second.slice(i+1,second.length)
+            first=first.split(',');
+          break;  
+        }
+    }
+    first=first.join('');
+    return first;
+}
+console.log( stringMerge("hello", "world", "l"));
+console.log( stringMerge("coding", "anywhere", "n") )
+console.log( stringMerge("jason", "samson", "s"))
 
 
 // 3
 
 
-//  function createPhoneNumber(numbers) {
-//     const begging = numbers.slice(0, 3);
-//     const middle = numbers.slice(3, 6);
-//     const end = numbers.slice(6, numbers.length);
-//     console.log('(' + begging.join('') + ')' + ' ' + middle.join('') + '-' + end.join(''));
-// }
+ function createPhoneNumber(numbers) {
+    const begging = numbers.slice(0, 3);
+    const middle = numbers.slice(3, 6);
+    const end = numbers.slice(6);
+    console.log('(' + begging.join('') + ')' + ' ' + middle.join('') + '-' + end.join(''));
+}
 
-// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
 
 
 // 4
 
-// function getOperands(numb){
-//     numb=numb.split(' ');
-//     return('a:' + numb[0] + 'b:'+ numb[2]);
-// }
+export function getOperands(numb){
+    numb=numb.split(' ');
+    return('a:' + numb[0] + 'b:'+ numb[2]);
+}
 
-// console.log (getOperands('1 + 2'));     
-// console.log (getOperands('1.2 * -3.4'));       
-// console.log (getOperands('1 - 2'));     
-// console.log (getOperands('1 - -2'));
+console.log (getOperands('1 + 2'));     
+console.log (getOperands('1.2 * -3.4'));       
+console.log (getOperands('1 - 2'));     
+console.log (getOperands('1 - -2'));
 
 // 5
 
 
 
 export function getTime(str) {
-    let result = str.match(/(0[0-9]|1[0-9]|2[0-3]|[0-9]):[0-5][0-9]/g)
+    const result = str.match(/(0[0-9]|1[0-9]|2[0-3]|[0-9]):[0-5][0-9]/g)
     return result[0]
 }
+
+
+// 6
+
+
+
+
+export  function isPalindrome(letter) {
+    let ans;
+    letter = letter.replace(/[^a-zA-Z]+/g, '').toLowerCase();
+    let newLetter = letter.split("").reverse().join("");
+    if(newLetter == letter){
+        ans = true;
+    }else
+    {
+        ans = false;
+    }
+    return ans;
+}   
+console.log(isPalindrome('Eva, can I see bees in a cave?')); 
+
+
+
+// 7
+
+
+
+
+export function swapCase(regExp) {
+    return( regExp.replace(/./g,a=>a==a.toUpperCase() ?  a.toLowerCase() : a.toUpperCase()))
+}
+
+
+
+swapCase("aBc");            //"AbC"
+swapCase("GooD");         //"gOOd"
+swapCase("hello");         //"HELLO"
+swapCase("75386");        //"75386"
+
+
+//8
+
+
+
+export function countOccurrences(word,letter){
+    let count=0;
+    
+    word = word.toLowerCase();
+    letter = letter.toLowerCase();
+
+    for(let i = 0; i < word.length; i++){
+        
+        if(word[i]==letter){
+            count++;
+        }
+    }
+    return(count);
+
+};
+
+
+countOccurrences('ability', 'i') ;        // 2
+countOccurrences('abc', 'a')             //1
+countOccurrences('ABC', 'a')             //1
+
+
+//9
+
+
+
+
+
+// 10
+ export function alphabetPosition(letter) {
+    const arr = []
+    letter = letter.replace(/[^a-zA-Z]+/g, '').toLowerCase();
+
+    for (let i = 0; i < letter.length; i++) {
+        arr.push(letter[i].charCodeAt(0) - 96);
+    }
+    return arr.join(' ');
+}
+
+alphabetPosition("The sunset sets at twelve o' clock.")               // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
--- index.test.js
+++ index.test.js
@@ -1,94 +1,187 @@
+import { basicOp } from './index'
+import { stringMerge } from './index'
+import { createPhoneNumber } from './index'
+import { getOperands } from './index'
+import { getTime } from './index'
+import{isPalindrome} from './index'
+import { swapCase } from './index'
+import { countOccurrences } from './index'
+import { vowelCount } from './index'
+import { alphabetPosition } from './index'
 
 // 1
 
 
-// import { basicOp } from './index'
-// describe('basicOp', function () {
-//     test('should return 4 when passed +, 2, 2', function () {
-//         const result = basicOp('+', 2, 2)
-//         expect(result).toBe(4)
-//     })
 
-//     test('should return -3 when passed -, 15, 18', function () {
-//         const result = basicOp('-', 15, 18)
-//         expect(result).toBe(-3)
-//     })
+describe('basicOp', function () {
+    test('should return 4 when passed +, 2, 2', function () {
+        const result = basicOp('+', 2, 2)
+        expect(result).toBe(4)
+    })
 
-//     test('should return 25 when passed *, 5, 5', function () {
-//         const result = basicOp('*', 5, 5)
-//         expect(result).toBe(25)
-//     })
+    test('should return -3 when passed -, 15, 18', function () {
+        const result = basicOp('-', 15, 18)
+        expect(result).toBe(-3)
+    })
 
-//     test('should return 7 when passed /, 49, 7', function () {
-//         const result = basicOp('/', 49, 7)
-//         expect(result).toBe(7)
-//     })
+    test('should return 25 when passed *, 5, 5', function () {
+        const result = basicOp('*', 5, 5)
+        expect(result).toBe(25)
+    })
 
-//     test('should return 0 when passed any, 45, 45', function () {
-//         const result = basicOp('something else', 45, 45)
-//         expect(result).toBe(0)
-//     })
-// })
+    test('should return 7 when passed /, 49, 7', function () {
+        const result = basicOp('/', 49, 7)
+        expect(result).toBe(7)
+    })
+
+    test('should return 0 when passed any, 45, 45', function () {
+        const result = basicOp('something else', 45, 45)
+        expect(result).toBe(0)
+    })
+})
 
 // 2
 
 
-// import { stringMerge } from './index'
-// describe('stringMerge', function () {
-//     test('should return held when passed "hello", "world", "l"', function () {
-//         const result = stringMerge("hello", "world", "l")
-//         expect(result).toBe('held')
-//     })
 
-//     test('should return codinywhere when passed "coding", "anywhere", "n"', function () {
-//         const result = stringMerge("coding", "anywhere", "n")
-//         expect(result).toBe('codinywhere')
-//     })
+describe('stringMerge', function () {
+    test('should return held when passed "hello", "world", "l"', function () {
+        const result = stringMerge("hello", "world", "l")
+        expect(result).toBe('held')
+    })
 
-//     test('should return jasamson when passed "jason", "samson", "s"', function () {
-//         const result = stringMerge("jason", "samson", "s")
-//         expect(result).toBe('jasamson')
-//     })
+    test('should return codinywhere when passed "coding", "anywhere", "n"', function () {
+        const result = stringMerge("coding", "anywhere", "n")
+        expect(result).toBe('codinywhere')
+    })
 
-//     test('should return wondeople when passed "wonderful", "people", "e"', function () {
-//         const result = stringMerge("wonderful", "people", "e")
-//         expect(result).toBe('wondeople')
-//     })
-// })
+    test('should return jasamson when passed "jason", "samson", "s"', function () {
+        const result = stringMerge("jason", "samson", "s")
+        expect(result).toBe('jasamson')
+    })
+
+    test('should return wondeople when passed "wonderful", "people", "e"', function () {
+        const result = stringMerge("wonderful", "people", "e")
+        expect(result).toBe('wondeople')
+    })
+})
 
 
 // 3
 
-// import { createPhoneNumber } from './index'
-// describe('createPhoneNumber', function () {
-//     test('should return "(123) 456-7890" when passed [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]', function () {
-//         const result = createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
-//         expect(result).toBe('(123) 456-7890')
-//     })
-// })
+
+describe('createPhoneNumber', function () {
+    test('should return "(123) 456-7890" when passed [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]', function () {
+        const result = createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
+        expect(result).toBe('(123) 456-7890')
+    })
+})
 
 
 
 // 4
 
+describe('getOperands', function () {
+    test('should return "a: 1, b: 2" when passed "1 + 2"', function () {
+        const numb = getOperands('1 + 2')
+        expect(numb).toBe('a: 1, b: 2')
+    })
 
+    test('should return "a: 1.2, b: -3.4" when passed "1.2 * -3.4"', function () {
+        const numb = getOperands('1.2 * -3.4')
+        expect(numb).toBe('a: 1.2, b: -3.4')
+    })
+
+    test('should return "a: 1, b: 2" when passed "1 - 2"', function () {
+        const numb = getOperands('1 - 2')
+        expect(numb).toBe('a: 1, b: 2')
+    })
+
+    test('should return "a: 1, b: -2" when passed "1 - -2"', function () {
+        const numb = getOperands('1 - -2')
+        expect(numb).toBe('a: 1, b: -2')
+    })
+})
 
 // 5
 
-// import { getTime } from './index'
-// describe('getTime', function () {
-//     test('should return "09:00" when passed "Breakfast at 09:00"', function () {
-//         const result = getTime('Breakfast at 09:00')
-//         expect(result).toBe('09:00')
-//     })
 
-//     test('should return "21:00" when passed "Breakfast at 09:60, Dinner at 21:00"', function () {
-//         const result = getTime('Breakfast at 09:60, Dinner at 21:00')
-//         expect(result).toBe('21:00')
-//     })
+describe('getTime', function () {
+    test('should return "09:00" when passed "Breakfast at 09:00"', function () {
+        const result = getTime('Breakfast at 09:00')
+        expect(result).toBe('09:00')
+    })
 
-//     test('should return "09:59" when passed "Breakfast at 09:59, Dinner at 21:00"', function () {
-//         const result = getTime('Breakfast at 09:59, Dinner at 21:00')
-//         expect(result).toBe('09:59')
-//     })
-// })
+    test('should return "21:00" when passed "Breakfast at 09:60, Dinner at 21:00"', function () {
+        const result = getTime('Breakfast at 09:60, Dinner at 21:00')
+        expect(result).toBe('21:00')
+    })
+
+    test('should return "09:59" when passed "Breakfast at 09:59, Dinner at 21:00"', function () {
+        const result = getTime('Breakfast at 09:59, Dinner at 21:00')
+        expect(result).toBe('09:59')
+    })
+    test('should return empty string when time is wrong', function() {
+        const result = getTime('Breakfast at 37:98')
+        expect(result).toBe('undefined')
+    })
+})
+
+// 6
+
+
+
+
+
+// 7
+
+
+
+describe('swapCase', function () {
+    test('should return AbC when passed "aBc"', function () {
+        const result = swapCase('aBc')
+        expect(result).toBe('AbC')
+    })
+
+    test('should return gOOd when passed "GooD"', function () {
+        const result = swapCase('GooD')
+        expect(result).toBe('gOOd')
+    })
+
+    test('should return HELLO when passed "hello"', function () {
+        const result = swapCase('hello')
+        expect(result).toBe('HELLO')
+    })
+})
+
+
+
+
+// 8
+
+describe('countOccurrences', function () {
+    test('should return 2 when passed "ability", "i"', function () {
+        const result = countOccurrences('ability', 'i')
+        expect(result).toBe(2)
+    })
+
+    test('should return 1 when passed "ability", "i"', function () {
+        const result = countOccurrences('abc', 'a')
+        expect(result).toBe(1)
+    })
+
+    test('should return 1 when passed "ability", "i"', function () {
+        const result = countOccurrences('ABC', 'a')
+        expect(result).toBe(1)
+    })
+})
+
+
+// 10g
+
+describe('alphabetPosition', function () {
+    test('should return 5 when passed "The sunset sets at twelve o`clock."', function () {
+        const result = alphabetPosition('The sunset sets at twelve o`clock.')
+        expect(result).toBe("20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11")
+    })
+})
